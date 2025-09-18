3b7a18f6d0afa311578d605f2c66c67f24253cb1
Neil Lin <neillin164@gmail.com>
2025-09-18 13:33:15 -0700

    bad code

--- csrc/bpe.cpp
+++ csrc/bpe.cpp
@@ -1,18 +1,35 @@
+#include <__atomic/aliases.h>
 #include <_types/_uint16_t.h>
 #include <algorithm>
 #include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <queue>
 #include <sstream>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 #include <chrono>
+#include <streambuf>
+#include <cstdio>
 
 #include <benchmark/benchmark.h>
 #include "robin_hood.h"
 
 #include <unicode/regex.h>
+#include <boost/heap/d_ary_heap.hpp>
+
+#include "ref.hpp"
+
+template <class T>
+using Heap = boost::heap::d_ary_heap<
+    T,
+    boost::heap::arity<4>,
+    boost::heap::mutable_<true>,
+    boost::heap::compare<std::less<T>>
+>;
+
 
 
 
@@ -45,7 +62,6 @@ struct PairHash {
   std::size_t operator()(const std::pair<T1, T2> &p) const {
     auto h1 = std::hash<T1>{}(p.first);
     auto h2 = std::hash<T2>{}(p.second);
-
     return h1 ^ (h2 << 1);
   }
 };
@@ -54,12 +70,72 @@ struct PairHash {
 
 
 constexpr uint16_t nsize = num_merges + 256 + 1;
-void byte_pair_enc(std::vector<std::vector<uint16_t>> &bytes, robin_hood::unordered_map<std::pair<uint16_t, uint16_t>, long long, PairHash> &freqs) {
+void byte_pair_enc(
+    std::vector<std::vector<uint16_t>> &bytes,
+    Heap<std::pair<long long, std::pair<std::uint16_t, std::uint16_t>>> &pq,
+    robin_hood::unordered_map<std::pair<uint16_t, uint16_t>,
+                              decltype(pq.push({})), PairHash> &keys) {
   const uint16_t N = (int)bytes.size();
 
-  // robin_hood::unordered_map<uint16_t, std::vector<uint16_t>> decoder_dict;
-  // robin_hood::unordered_map<std::pair<uint16_t, uint16_t>, uint16_t, PairHash>
-  //     encoder_dict;
+  const uint16_t start_idx = 256;
+  std::vector<long long> pos;
+  robin_hood::unordered_map<std::pair<uint16_t, uint16_t>, long long, PairHash> diffs;
+
+  for (uint16_t it = 0; it < num_merges; it++) {
+    const uint16_t new_idx = it + start_idx;
+
+    const std::pair<long long, std::pair<uint16_t, uint16_t>>& most_freq = pq.top();
+
+    if (most_freq.first <= 1)
+      break;
+    std::pair<uint16_t, uint16_t> mf_vals = most_freq.second;
+
+    for (int b = 0; b < N; b++) {
+      auto& enc_x = bytes[b];
+      std::vector<uint16_t> nenc_x;
+      nenc_x.reserve(enc_x.size());
+
+      for (uint16_t i = 0, sz = (int)enc_x.size(); i < sz - 1; i++) {
+        if (enc_x[i] == mf_vals.first && enc_x[i + 1] == mf_vals.second) {
+          nenc_x.push_back(new_idx);
+          pos.push_back((int)nenc_x.size() - 1);
+          if (i - 1 >= 0) {
+            diffs[{enc_x[i-1], new_idx}]++;
+            diffs[{enc_x[i-1], mf_vals.first}]--;
+          }
+          i++;
+        } else {
+          nenc_x.push_back(enc_x[i]);
+        }
+      }
+      enc_x = nenc_x;
+
+      for (auto i : pos) {
+        if (i+1 < (int)enc_x.size()) {
+          if (enc_x[i+1] != new_idx)
+            diffs[{mf_vals.second, enc_x[i + 1]}]--;
+          else 
+            diffs[{mf_vals.second, mf_vals.first}]--;
+          diffs[{new_idx, enc_x[i + 1]}]++;
+        }
+      }
+      pos.clear();
+    }
+
+    for (auto [k, v] : diffs) {
+      if (keys.contains(k)) {
+        auto handle = keys[k];
+        pq.update(handle, {(*handle).first + v, k});
+      } else {
+        keys[k] = pq.push({v, k});
+      }
+    }
+    diffs.clear();
+  }
+}
+
+void byte_pair_enc_old(std::vector<std::vector<uint16_t>> &bytes, robin_hood::unordered_map<std::pair<uint16_t, uint16_t>, long long, PairHash> &freqs) {
+  const uint16_t N = (int)bytes.size();
 
   const uint16_t start_idx = 256;
   std::vector<long long> pos;
@@ -67,6 +143,7 @@ void byte_pair_enc(std::vector<std::vector<uint16_t>> &bytes, robin_hood::unorde
   for (uint16_t it = 0; it < num_merges; it++) {
     const uint16_t new_idx = it + start_idx;
 
+    // std::priority_queue<std::pair<long long, std::pair<uint16_t, uint16_t>>> pq;
     std::pair<long long, std::pair<uint16_t, uint16_t>> most_freq; // idea: use a priority queue for this
     most_freq = {0, {0, 0}};
     for (auto &[k, v] : freqs)
@@ -75,14 +152,6 @@ void byte_pair_enc(std::vector<std::vector<uint16_t>> &bytes, robin_hood::unorde
     if (most_freq.first <= 1)
       break;
     std::pair<uint16_t, uint16_t> mf_vals = most_freq.second;
-
-    // std::vector<uint16_t> nkey;
-    // nkey.insert(nkey.end(), decoder_dict[mf_vals.first].begin(),
-    //             decoder_dict[mf_vals.first].end());
-    // nkey.insert(nkey.end(), decoder_dict[mf_vals.second].begin(),
-    //             decoder_dict[mf_vals.second].end());
-    // decoder_dict[new_idx] = nkey;
-    // encoder_dict[mf_vals] = new_idx;
 
     for (int b = 0; b < N; b++) {
       auto& enc_x = bytes[b];
@@ -115,11 +184,9 @@ void byte_pair_enc(std::vector<std::vector<uint16_t>> &bytes, robin_hood::unorde
       }
       pos.clear();
     }
-    freqs.erase(mf_vals); // no longer possible to get this key
+    freqs.erase(mf_vals); 
   }
 }
-
-
 
 
 
@@ -142,11 +209,57 @@ std::vector<std::string> regex_split(const std::vector<std::string>& in) {
   return matches;
 }
 
-
-
-
+struct OutputSilencer {
+  std::streambuf *old_out{nullptr}, *old_err{nullptr};
+  OutputSilencer() {
+    static struct : std::streambuf { int overflow(int c) override { return c; } } nullbuf;
+    old_out = std::cout.rdbuf(&nullbuf);
+    old_err = std::cerr.rdbuf(&nullbuf);
+  }
+  ~OutputSilencer() {
+    std::cout.rdbuf(old_out);
+    std::cerr.rdbuf(old_err);
+    fflush(stdout); fflush(stderr);
+  }
+};
 
 static void BM_BPE(benchmark::State &state) {
+  std::ifstream t("data/input.txt");
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+
+  std::vector<std::string> in{buffer.str()};
+  std::vector<std::string> matches = regex_split(in);
+
+  for (auto _ : state) {
+    // state.PauseTiming();
+
+    robin_hood::unordered_map<std::pair<uint16_t, uint16_t>, long long, PairHash> freqs;
+    std::vector<std::vector<uint16_t>> bytes;
+    bytes.reserve((int)matches.size());
+    std::transform(matches.begin(), matches.end(), std::back_inserter(bytes),
+                  [](std::string &s) { return (to_bytes(s)); });
+
+    for (auto byte : bytes)
+      for (uint16_t i = 0, sz = (int)byte.size(); i < sz - 1; i++)
+        freqs[{byte[i], byte[i + 1]}]++;
+
+    Heap<std::pair<long long, std::pair<std::uint16_t, std::uint16_t>>> pq;
+    robin_hood::unordered_map<std::pair<uint16_t, uint16_t>,
+                              decltype(pq.push({})), PairHash>
+        keys;
+    for (auto [k, v] : freqs) {
+      keys[k] = pq.push({v, k});
+    }
+    // state.ResumeTiming();
+
+    byte_pair_enc(bytes, pq, keys);
+    // byte_pair_enc_old(bytes, freqs);
+    benchmark::DoNotOptimize(bytes);
+  }
+}
+
+static void BM_BPE_OLD(benchmark::State &state) {
   std::ifstream t("data/input.txt");
   std::stringstream buffer;
   buffer << t.rdbuf();
@@ -168,13 +281,23 @@ static void BM_BPE(benchmark::State &state) {
         freqs[{byte[i], byte[i + 1]}]++;
     state.ResumeTiming();
 
-    byte_pair_enc(bytes, freqs);
+    byte_pair_enc_old(bytes, freqs);
     benchmark::DoNotOptimize(bytes);
   }
 }
 
+static void BM_REF(benchmark::State &state){
+  for(auto _ : state){
+    OutputSilencer mute; 
+    fastBPE::learnbpe(100, "data/input.txt", "");
+  }
+}
+
 BENCHMARK(BM_BPE);
+BENCHMARK(BM_BPE_OLD);
+BENCHMARK(BM_REF);
 BENCHMARK_MAIN();
+
 
 /*
 todo: need to split the function into smaller parts? 
@@ -185,4 +308,3 @@ when looking for pairs use offset array, and when replacing we increment offset
 figure out how to use dp and tries to optimize the code
 
 */
-//  \/\/ hello world
